326202525fc56996abe335e0b73546b183ecca0f
subhajit chowdhury <125207665+subhajitchowdhury07@users.noreply.github.com>
2024-10-21 08:53:22 +0530

    Time: 1 ms (19.49%), Space: 45.7 MB (82.8%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,39 +1,35 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        // If the array is empty, return [-1, -1]
-        if (nums.length == 0) {
-            return new int[]{-1, -1};
+        if(nums.length==0){
+            return new int[]{-1,-1};
         }
 
-        int p1 = -1; // Start position
-        int p2 = -1; // End position
-        int[] arr = new int[2];
+        int p1=-1;
+        int p2=-1;
+        int arr[] = new int[2];
 
-        // Find the first occurrence of target
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target) {
-                p1 = i;
+        for(int i=0; i<nums.length;i++){
+            if(nums[i] == target){
+                p1=i;
                 break;
             }
         }
 
-        // Find the last occurrence of target
-        for (int j = nums.length - 1; j >= 0; j--) {
-            if (nums[j] == target) {
-                p2 = j;
+        for(int j=nums.length-1; j>=0; j--){
+            if(nums[j]== target){
+                p2= j;
                 break;
             }
         }
-
-        // If target is found, set p1 and p2 in the result array
-        if (p1 != -1 && p2 != -1) {
-            arr[0] = p1;
-            arr[1] = p2;
-        } else {
-            arr[0] = -1;
-            arr[1] = -1;
+        if(p1 != -1 && p2 != -1){
+            arr[0]= p1;
+            arr[1]= p2;
+        }else{
+            arr[0]= -1;
+            arr[1]= -1;
         }
 
         return arr;
+        
     }
 }
